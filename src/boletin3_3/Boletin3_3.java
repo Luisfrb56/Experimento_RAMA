13b8565c8ef86af086aa7edad79961eaee567ebe
adm <adm@LAPTOP-EQL1FQIM>
2017-10-24 08:41:21 +0200

    Circulo boletin 3_3

---
+++ src/boletin3_3/Boletin3_3.java
@@ -0,0 +1,20 @@
+
+package boletin3_3;
+
+import java.util.Scanner;
+
+public class Boletin3_3 {
+
+    public static void main(String[] args) {
+        Circulo numero1=new Circulo();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Dime el radio: ");
+        double radio=sc.nextDouble();
+        Circulo numero2 =new Circulo(radio); 
+        numero2.calcularArea();
+        numero2.calcularLonxiturde();
+        System.out.println(numero2.calcularArea());
+        System.out.println(numero2.calcularLonxiturde());
+    }
+    
+}
